a00d909c5d313c62f66090df5e527d10f4807a7e
Trammell Hudson <hudson@trmm.net>
2017-04-12 06:50:18 -0400

    copy file and compute sha256 before flashing Signed-off-by: Thierry Laurion <insurgo@riseup.net>

--- initrd/bin/flashrom-x230.sh
+++ initrd/bin/flashrom-x230.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+. /etc/functions
+
+ROM="$1"
+if [ -z "$1" ]; then
+	die "Usage: $0 /media/x230.rom"
+fi
+
+cp "$ROM" /tmp/x230.rom
+sha256sum /tmp/x230.rom
 
 flashrom \
 	--force \
@@ -6,4 +15,8 @@ flashrom \
 	--programmer internal \
 	--layout /etc/x230-layout.txt \
 	--image BIOS \
-	-w "$*"
+	-w /tmp/x230.rom \
+|| die "$ROM: Flash failed"
+
+warn "Reboot and hopefully it works..."
+exit 0
