1cbae3cc12cae7c90b263a02d746c2f25e1a5e0d
Francis Lam <flam@alum.mit.edu>
2018-04-21 17:21:37 -0700

    Preserve custom CBFS files on flashrom updates

--- initrd/bin/flashrom-x230.sh
+++ initrd/bin/flashrom-x230.sh
@@ -1,13 +1,24 @@
 #!/bin/sh
 . /etc/functions
 
-ROM="$1"
-if [ -z "$1" ]; then
-	die "Usage: $0 /media/x230.rom"
+if [ "$1" = "-c" ]; then
+	CLEAN=1
+	ROM="$2"
+else
+	CLEAN=0
+	ROM="$1"
+fi
+
+if [ ! -e "$ROM" ]; then
+	die "Usage: $0 [-c] /media/x230.rom"
 fi
 
 cp "$ROM" /tmp/x230.rom
 sha256sum /tmp/x230.rom
+if [ "$CLEAN" -eq 0 ]; then
+	preserve_rom /tmp/x230.rom \
+	|| die "$ROM: Config preservation failed"
+fi
 
 flashrom \
 	--force \
