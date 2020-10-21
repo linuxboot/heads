08d620b675868be4e02c433ec8576e5aae0b306e
Tom Hiller <thrilleratplay@gmail.com>
2020-10-21 16:54:55 -0400

    chore(initrd): fix hidden errors

--- initrd/bin/mount-usb.sh
+++ initrd/bin/mount-usb.sh
@@ -5,15 +5,17 @@
 enable_usb
 
 if ! lsmod | grep -q usb_storage; then
-  count=$(ls /dev/sd* 2>/dev/null | wc -l)
+  init_dev_count=$(find /dev/sd* 2>/dev/null | wc -l)
+  count=$init_dev_count
   timeout=0
   echo "Scanning for USB storage devices..."
   insmod /lib/modules/usb-storage.ko >/dev/null 2>&1 \
   || die "usb_storage: module load failed"
-  while [[ $count == $(ls /dev/sd* 2>/dev/null | wc -l) ]]; do
-    [[ $timeout -ge 4 ]] && break
+  while [ $((init_dev_count)) -eq $((count)) ]; do
+    [ $((timeout)) -ge 4 ] && break
     sleep 1
-    timeout=$(($timeout+1))
+    timeout=$((timeout+1))
+    count=$(find /dev/sd* 2>/dev/null | wc -l)
   done
 fi
 
@@ -21,20 +23,20 @@ if [ ! -d /media ]; then
   mkdir /media
 fi
 
-stat -c %N /sys/block/sd* 2>/dev/null | grep usb | cut -f1 -d ' ' | sed "s/[']//g;s|/sys/block|/dev|" > /tmp/usb_block_devices
-if [ -z $(cat /tmp/usb_block_devices) ]; then
+USB_BLOCK_DEV_COUNT=$(stat -c %N /sys/block/sd* 2>/dev/null | grep usb | cut -f1 -d ' ' | sed "s/[']//g;s|/sys/block|/dev|" | wc -l)
+if [ $((USB_BLOCK_DEV_COUNT)) -eq 0 ]; then
   if [ -x /bin/whiptail ]; then
     whiptail --title 'USB Drive Missing' \
       --msgbox "Insert your USB drive and press Enter to continue." 16 60
   else
     echo "+++ USB Drive Missing! Insert your USB drive and press Enter to continue."
-    read
+    read -r
   fi
   sleep 1
-  stat -c %N /sys/block/sd* 2>/dev/null | grep usb | cut -f1 -d ' ' | sed "s/[']//g;s|/sys/block|/dev|" > /tmp/usb_block_devices
-  if [ -z $(cat /tmp/usb_block_devices) ]; then
+  USB_BLOCK_DEV_COUNT=$(stat -c %N /sys/block/sd* 2>/dev/null | grep usb | cut -f1 -d ' ' | sed "s/[']//g;s|/sys/block|/dev|"| wc -l)
+  if [ $((USB_BLOCK_DEV_COUNT)) -eq 0 ]; then
     if [ -x /bin/whiptail ]; then
-      whiptail $CONFIG_ERROR_BG_COLOR --title 'ERROR: USB Drive Missing' \
+      whiptail "$CONFIG_ERROR_BG_COLOR" --title 'ERROR: USB Drive Missing' \
         --msgbox "USB Drive Missing! Aborting mount attempt.\n\nPress Enter to continue." 16 60
     else
       echo "!!! ERROR: USB Drive Missing! Aborting mount. Press Enter to continue."
@@ -45,27 +47,33 @@ fi
 
 USB_MOUNT_DEVICE=""
 # Check for the common case: a single USB disk with one partition
-if [ $(wc -l /tmp/usb_block_devices) -eq 1 ]; then
-  USB_BLOCK_DEVICE=$(cat /tmp/usb_block_devices)
+USB_BLOCK_DEV_COUNT=$(wc -l /tmp/usb_block_devices)
+if [ $((USB_BLOCK_DEV_COUNT)) -eq 1 ]; then
+  USB_BLOCK_DEVICES=$(cat /tmp/usb_block_devices)
+  USB_NUM_PARTITIONS=$(find "$USB_BLOCK_DEVICES"* | wc -l)
   # Subtract out block device
-  let USB_NUM_PARTITIONS=$(($(ls -1 ${USB_BLOCK_DEVICE}* | wc -l) -1))
-  if [ ${USB_NUM_PARTITIONS} -eq 0 ]; then
-    USB_MOUNT_DEVICE=${USB_BLOCK_DEVICE}
-  elif [ ${USB_NUM_PARTITIONS} -eq 1 ]; then
-    USB_MOUNT_DEVICE=$(ls -1 ${USB_BLOCK_DEVICE}* | tail -n1)
+  USB_NUM_PARTITIONS=$((USB_NUM_PARTITIONS-1))
+  if [ $((USB_NUM_PARTITIONS)) -eq 0 ]; then
+    USB_MOUNT_DEVICE="$USB_BLOCK_DEVICES"
+  elif [ $((USB_NUM_PARTITIONS)) -eq 1 ]; then
+    USB_MOUNT_DEVICE=$(find "$USB_BLOCK_DEVICES"* | tail -n1)
   fi
 fi
 # otherwise, let the user pick
-if [ -z ${USB_MOUNT_DEVICE} ]; then
-  > /tmp/usb_disk_list
-  for i in $(cat /tmp/usb_block_devices); do
+if [ -z "$USB_MOUNT_DEVICE" ]; then
+  # > /tmp/usb_disk_list
+  USB_BLOCK_DEVICES=$(cat /tmp/usb_block_devices)
+  USB_NUM_PARTITIONS=$(find "$USB_BLOCK_DEVICES"* | wc -l)
+  for i in $USB_BLOCK_DEVICES; do
     # remove block device from list if numeric partitions exist, since not bootable
-    let USB_NUM_PARTITIONS=$(($(ls -1 $i* | wc -l)-1))
-    if [ ${USB_NUM_PARTITIONS} -eq 0 ]; then
-      echo $i $(blkid | grep $i | grep -o 'LABEL=".*"' | cut -f2 -d '"') >> /tmp/usb_disk_list
+    USB_NUM_PARTITIONS=$((USB_NUM_PARTITIONS-1))
+    if [ $((USB_NUM_PARTITIONS)) -eq 0 ]; then
+      USB_DISK=$(blkid | grep "$i" | grep -o 'LABEL=".*"' | cut -f2 -d '"')
+      echo "$i $USB_DISK" >> /tmp/usb_disk_list
     else
-      for j in $(ls $i* | tail -${USB_NUM_PARTITIONS}); do
-        echo $j $(blkid | grep $j | grep -o 'LABEL=".*"' | cut -f2 -d '"') >> /tmp/usb_disk_list
+      for j in $(find "$i*" | tail -$((USB_NUM_PARTITIONS))); do
+        USB_DISK=$(blkid | grep "$j" | grep -o 'LABEL=".*"' | cut -f2 -d '"')
+        echo "$j $USB_DISK" >> /tmp/usb_disk_list
       done
     fi
   done
@@ -73,30 +81,30 @@ if [ -z ${USB_MOUNT_DEVICE} ]; then
   if [ -x /bin/whiptail ]; then
     MENU_OPTIONS=""
     n=0
-    while read option
+    while read -r option
     do
       n=$((n + 1))
-      option=$(echo $option | tr " " "_")
+      option=$(echo "$option" | tr " " "_")
       MENU_OPTIONS="$MENU_OPTIONS $n ${option}"
     done < /tmp/usb_disk_list
 
     MENU_OPTIONS="$MENU_OPTIONS a Abort"
     whiptail --clear --title "Select your USB disk" \
       --menu "Choose your USB disk [1-$n, a to abort]:" 20 120 8 \
-      -- $MENU_OPTIONS \
+      -- "$MENU_OPTIONS" \
       2>/tmp/whiptail
 
     option_index=$(cat /tmp/whiptail)
   else
     echo "+++ Select your USB disk:"
     n=0
-    while read option
+    while read -r option
     do
       n=$((n + 1))
       echo "$n. $option"
     done < /tmp/usb_disk_list
 
-    read \
+    read -r \
       -p "Choose your USB disk [1-$n, a to abort]: " \
       option_index
   fi
@@ -104,11 +112,11 @@ if [ -z ${USB_MOUNT_DEVICE} ]; then
   if [ "$option_index" = "a" ]; then
     exit 5
   fi
-  USB_MOUNT_DEVICE=$(head -n $option_index /tmp/usb_disk_list | tail -1 | sed 's/\ .*$//')
+  USB_MOUNT_DEVICE=$(head -n $((option_index)) /tmp/usb_disk_list | tail -1 | sed 's/\ .*$//')
 fi
 
 if [ "$1" = "rw" ]; then
-  mount -o rw $USB_MOUNT_DEVICE /media
+  mount -o rw "$USB_MOUNT_DEVICE" /media
 else
-  mount -o ro $USB_MOUNT_DEVICE /media
+  mount -o ro "$USB_MOUNT_DEVICE" /media
 fi
