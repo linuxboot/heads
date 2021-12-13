4b73f3d206b4f27f854e4fa6e9ac923a2c44f19a
eganonoa <56741779+eganonoa@users.noreply.github.com>
2021-12-13 18:25:41 +0100

    Add dGPU support for t530 and w530, including rom extraction script
    Tested on t530's with and without the dGPU and the w530 with K1000M Nvidia Quadro dGPU.   The rom extraction script has been tested in Debian 10 and Ubuntu 21.04

---
+++ blobs/xx30/vbios_w530.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install wget ruby ruby-dev ruby-bundler p7zip-full upx-ucl 
+
+git clone https://github.com/awilliam/rom-parser.git
+cd rom-parser
+make
+sudo cp rom-parser /usr/sbin/
+cd ..
+sudo rm -r rom-parser
+
+wget https://github.com/LongSoft/UEFITool/releases/download/A58/UEFIExtract_NE_A58_linux_x86_64.zip
+unzip UEFIExtract_NE_A58_linux_x86_64.zip
+sudo mv UEFIExtract /usr/sbin/
+rm UEFIExtract_NE_A58_linux_x86_64.zip
+
+git clone https://github.com/coderobe/VBiosFinder.git
+cd VBiosFinder
+bundle install --path=vendor/bundle
+wget https://download.lenovo.com/pccbbs/mobiles/g5uj39us.exe -P /home/$USER/
+./vbiosfinder extract /home/$USER/g5uj39us.exe
+rm /home/$USER/g5uj39us.exe
+cd output
+mv vbios_10de_0ffb_1.rom ../../10de,0ffb.rom
+mv vbios_10de_0ffc_1.rom ../../10de,0ffc.rom
+mv vbios_8086_0106_1.rom ../../8086,0106.rom
+cd ..
+cd ..
+sudo rm -r VBiosFinder
+
