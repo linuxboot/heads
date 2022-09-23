fc276ba9afd1a706f3363fb2ce179f01fa09d65e
weyounsix <108412593+weyounsix@users.noreply.github.com>
2022-09-23 16:53:19 -0400

    xx30 vbios scripts error handling and better scoping of UEFIExtract ... (#3)
    …for FL1 files only. Also add sudo calls where required
    
    most of this work plagiarized from https://github.com/osresearch/heads/pull/1111
    (thanks @tlaurion [;)

--- blobs/xx30/vbios_t430.sh
+++ blobs/xx30/vbios_t430.sh
@@ -20,7 +20,7 @@ echo "### Installing basic dependencies"
 sudo apt update && sudo apt install -y wget ruby ruby-dev ruby-bundler p7zip-full upx-ucl 
 
 echo "### Downloading rom-parser dependency"
-wget https://github.com/awilliam/rom-parser/archive/"$ROMPARSER".zip
+wget https://github.com/awilliam/rom-parser/archive/"$ROMPARSER".zip || { echo "Failed to download $ROMPARSER" && exit 1; }
 
 echo "### Verifying expected hash of rom-parser"
 echo "$ROM_PARSER_SHA256SUM" | sha256sum --check || { echo "Failed sha256sum verification..." && exit 1; }
@@ -31,7 +31,7 @@ cd rom-parser-"$ROMPARSER" && make
 sudo cp rom-parser /usr/sbin/
 
 echo "### Downloading UEFIExtract dependency"
-wget https://github.com/LongSoft/UEFITool/releases/download/A58/"$UEFIEXTRACT"
+wget https://github.com/LongSoft/UEFITool/releases/download/A58/"$UEFIEXTRACT" || { echo "Failed to download $UEFIEXTRACT" && exit 1; }
 
 echo "### Verifying expected hash of UEFIExtract"
 echo "$UEFI_EXTRACT_SHA256SUM" | sha256sum --check || { echo "Failed sha256sum verification..." && exit 1; }
@@ -41,7 +41,7 @@ unzip "$UEFIEXTRACT"
 sudo mv UEFIExtract /usr/sbin/
 
 echo "### Downloading VBiosFinder"
-wget https://github.com/coderobe/VBiosFinder/archive/"$VBIOSFINDER".zip
+wget https://github.com/coderobe/VBiosFinder/archive/"$VBIOSFINDER".zip || { echo "Failed to download $VBIOSFINDER" && exit 1; }
 
 echo "### Verifying expected hash of VBiosFinder"
 echo "$VBIOS_FINDER_SHA256SUM" | sha256sum --check || { echo "Failed sha256sum verification..." && exit 1; }
@@ -51,13 +51,16 @@ unzip "$VBIOSFINDER".zip
 cd VBiosFinder-"$VBIOSFINDER" && bundle install --path=vendor/bundle
 
 echo "### Downloading latest Lenovo bios update for t430"
-wget https://download.lenovo.com/pccbbs/mobiles/"$BIOSUPDATE"
+wget https://download.lenovo.com/pccbbs/mobiles/"$BIOSUPDATE" || { echo "Failed to download $BIOSUPDATE" && exit 1; }
 
 echo "### Verifying expected hash of bios update"
 echo "$BIOS_UPDATE_SHA256SUM" | sha256sum --check || { echo "Failed sha256sum verification..." && exit 1; }
 
+echo "### Extracting bios update"
+innoextract "$extractdir"/rom-parser-"$ROMPARSER"/VBiosFinder-"$VBIOSFINDER"/"$BIOSUPDATE" || { echo "Failed to extract $BIOSUPDATE" && exit 1; }
+
 echo "### Finding, extracting and saving vbios"
-./vbiosfinder extract "$extractdir"/rom-parser-"$ROMPARSER"/VBiosFinder-"$VBIOSFINDER"/"$BIOSUPDATE"
+sudo ./vbiosfinder extract "$extractdir"/rom-parser-"$ROMPARSER"/VBiosFinder-"$VBIOSFINDER"/"app/G1ETC2WW/\$01D2000.FL1" || { echo "Failed to extract FL1" && exit 1; }
 
 echo "Verifying expected hash of extracted roms"
 cd output
@@ -65,9 +68,9 @@ echo "$DGPU_ROM_SHA256SUM" | sha256sum --check || { echo "dGPU rom failed sha256
 echo "$IGPU_ROM_SHA256SUM" | sha256sum --check || { echo "iGPU rom Failed sha256sum verification..." && exit 1; }
 
 echo "### Moving extracted roms to blobs directory"
-mv vbios_10de_0def_1.rom $BLOBDIR/10de,0def.rom
-mv vbios_8086_0106_1.rom $BLOBDIR/8086,0106.rom
+sudo mv vbios_10de_0def_1.rom $BLOBDIR/10de,0def.rom
+sudo mv vbios_8086_0106_1.rom $BLOBDIR/8086,0106.rom
 
 echo "### Cleaning Up"
 cd "$BLOBDIR"
-rm -rf "$extractdir"
+sudo rm -rf "$extractdir"
