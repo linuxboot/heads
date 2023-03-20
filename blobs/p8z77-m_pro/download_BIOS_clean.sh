4336100945ea3e3ddfbe778ebbec22f1042327c1
ThePlexus <31340423+ThePlexus@users.noreply.github.com>
2023-03-20 16:20:08 +0000

    change p8z77-m_pro binary blob process to avoid ifdtool

--- blobs/p8z77-m_pro/download_BIOS_clean.sh
+++ blobs/p8z77-m_pro/download_BIOS_clean.sh
@@ -16,11 +16,6 @@ while getopts ":m:i:" opt; do
         MECLEAN="$OPTARG"
       fi
       ;;
-    i)
-      if [ -x "$OPTARG" ]; then
-        IFDTOOL="$OPTARG"
-      fi
-      ;;
   esac
 
 done
@@ -33,19 +28,11 @@ if [ -z "$MECLEAN" ]; then
   fi
 fi
 
-if [ -z "$IFDTOOL" ]; then
-  IFDTOOL=`command -v $BLOBDIR/../../build/x86/coreboot-*/util/ifdtool/ifdtool 2>&1|head -n1`
-  if [ -z "$IFDTOOL" ]; then
-    echo "ifdtool required but not found or specified with -i. Aborting."
-    exit 1;
-  fi
-fi
-
 CAP_ZIP_SHA256SUM="9ea900eccd4a649237b000f1a34beb73cd92fb203d9639d8b7d22ef2a030d360  P8Z77-V-PRO-ASUS-2104.zip"
 CAP_FILE_SHA256SUM="7cf39a893cd6af774e3623a6b80c3e8f8989934b384eff28aba4726e80faa962  P8Z77-V-PRO-ASUS-2104.CAP"
-FINAL_IFD_SHA256SUM="dea50bf6577a0b36db54a62890c939d9c1a942eaf93c4479e00a9a2e48b8bf5d flashregion_0_flashdescriptor.bin"
+FINAL_IFD_SHA256SUM="f076be608c189da9532484b8152bc34029d1b1a8e28f630799fd474c47cb3f88 $BLOBDIR/ifd.bin"
 FINAL_ME_SHA256SUM="8dda1e8360fbb2da05bfcd187f6e7b8a272a67d66bc0074bbfd1410eb35e3e17 $BLOBDIR/me.bin"
-FINAL_GBE_SHA256SUM="fca4deb13633712113e1824bfd5afa32f487ca7129ca012fecf5d7502ec1d5ba  flashregion_3_gbe.bin"
+FINAL_GBE_SHA256SUM="fca4deb13633712113e1824bfd5afa32f487ca7129ca012fecf5d7502ec1d5ba  $BLOBDIR/gbe.bin"
 ZIPURL="https://dlcdnets.asus.com/pub/ASUS/mb/LGA1155/P8Z77-V_PRO/P8Z77-V-PRO-ASUS-2104.zip"
 
 ZIPFILENAME=`echo $ZIPURL | sed 's/.*\///'`
@@ -54,14 +41,6 @@ ROMFILENAME=`echo $ZIPFILENAME | sed 's/\.zip$/\.ROM/'`
 extractdir=$(mktemp -d)
 echo "### Creating temp dir $extractdir "
 cd "$extractdir"
-
-/bin/cat <<EOF > layout.txt
-00000000:00000fff fd
-0001b000:007fffff bios
-00003000:0001afff me
-00001000:00002fff gb
-EOF
-
 
 echo "### Downloading $ZIPURL"
 wget $ZIPURL || { echo "ERROR: wget failed $ZIPURL" && exit 1; }
@@ -77,32 +56,21 @@ echo "$CAP_FILE_SHA256SUM" | sha256sum --check || { echo "Failed sha256sum verif
 echo "### extracing BIOS from Capsule"
 dd bs=1024 skip=2 if=P8Z77-V-PRO-ASUS-2104.CAP of=P8Z77-V-PRO-ASUS-2104.ROM || { echo "Failed to de-cap the ROM..." && exit 1; }
 
-echo "### Stock variant AltME & ME Cleaner"
-$IFDTOOL -M 1 $ROMFILENAME
-echo "### extract stock ME"
-$IFDTOOL -x $ROMFILENAME.new
-
 echo "### Applying me_cleaner to neuter and truncate. EFFS,FCRS whitelisted"
-$MECLEAN -r -t -O "$BLOBDIR/me.bin" flashregion_2_intel_me.bin
+$MECLEAN -S -r -t -d -O  /tmp/unneeded.bin -D "$BLOBDIR/ifd.bin" -M "$BLOBDIR/me.bin" P8Z77-V-PRO-ASUS-2104.ROM
 
-echo "### Verifying expected hash of me.bin"
-echo "$FINAL_ME_SHA256SUM" | sha256sum --check || { echo "Failed sha256sum verification on final binary..." && exit 1; }
-rm flashregion*.bin
-echo "### Resizing ..."
-$IFDTOOL -D 8 $ROMFILENAME.new || { echo "Failed density resize " && exit 1;}
-$IFDTOOL --newlayout layout.txt $ROMFILENAME.new.new || { echo "Failed new layout ..." && exit 1;}
-echo "### Extracting final IFD"
-$IFDTOOL -x $ROMFILENAME.new.new.new || { echo "Failed ifdtool. Tool installed on host?" && exit 1;}
+echo "### Extracting GBE binary"
+dd of="$BLOBDIR/gbe.bin" if=P8Z77-V-PRO-ASUS-2104.ROM bs=1 skip=4096 count=8192 conv=notrunc iflag=skip_bytes,count_bytes
 
-printf '\x00' | dd of=flashregion_0_flashdescriptor.bin bs=1 seek=3837 count=1 conv=notrunc
-printf '\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF' | dd of=flashregion_0_flashdescriptor.bin bs=1 seek=3568 count=32 conv=notrunc
+echo "### Modifying VSCC length and identifiers"
 
-echo "### Verifying expected hash of IFD"
-echo "$FINAL_IFD_SHA256SUM" | sha256sum --check || { echo "Failed sha256sum verification on factory IFD bin..." && exit 1; }
-cp flashregion_0_flashdescriptor.bin $BLOBDIR/ifd.bin || { echo "Failed to copy IFD ..." && exit 1; }
-echo "$FINAL_GBE_SHA256SUM" | sha256sum --check || { echo "Failed sha256sum verification on factory IFD bin..." && exit 1; }
-cp flashregion_3_gbe.bin $BLOBDIR/gbe.bin || { echo "Failed to copy GBE ..." && exit 1; }
+printf '\x00' | dd of="$BLOBDIR/ifd.bin" bs=1 seek=3837 count=1 conv=notrunc
+printf '\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF' | dd of="$BLOBDIR/ifd.bin" bs=1 seek=3568 count=32 conv=notrunc
 
+echo "### Verifying expected hashes"
+echo "$FINAL_IFD_SHA256SUM" | sha256sum --check || { echo "Failed sha256sum verification on generated IFD bin..." && exit 1; }
+echo "$FINAL_GBE_SHA256SUM" | sha256sum --check || { echo "Failed sha256sum verification on extracted GBE bin..." && exit 1; }
+echo "$FINAL_ME_SHA256SUM" | sha256sum --check || { echo "Failed sha256sum verification on generated ME binary..." && exit 1; }
 
 echo "###Cleaning up..."
 cd -
