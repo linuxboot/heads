82e519112f92ce8d74e1137124f0b55509039da6
Markus Meissner <coder@safemailbox.de>
2023-08-07 17:27:40 +0200

    fake commit, circleci

--- create-npf.sh
+++ create-npf.sh
@@ -6,3 +6,5 @@ cd ./build/x86/${BOARD}/
 sha256sum heads-${BOARD}-${HEADS_GIT_VERSION}.rom > sha256sum.txt
 sed -ie 's@  @  /tmp/verified_rom/@g' sha256sum.txt
 zip heads-${BOARD}-${HEADS_GIT_VERSION}.npf heads-${BOARD}-${HEADS_GIT_VERSION}.rom sha256sum.txt
+
+# fake commit circleci
