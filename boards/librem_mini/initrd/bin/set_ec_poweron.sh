eed8adeb49897d89f49d9ac0e096f6e0299d0101
Jonathon Hall <jonathon.hall@puri.sm>
2023-08-29 15:55:20 -0400

    librem_mini,librem_mini_v2: Enable CMOS layout, update CMOS checksum
    Enable the coreboot CMOS option table, which initializes CMOS if the
    checksum is not valid.
    
    There is now a checksum in the CMOS layout since 4.21, update it when
    updating the Mini v1/v2 EC power-on setting.
    
    coreboot 4.21 will reset the CMOS settings during the first boot, since
    there was no checksum in prior releases.  Heads will restore the
    automatic power-on setting during init based on config.user.
    
    Signed-off-by: Jonathon Hall <jonathon.hall@puri.sm>

--- boards/librem_mini/initrd/bin/set_ec_poweron.sh
+++ boards/librem_mini/initrd/bin/set_ec_poweron.sh
@@ -7,10 +7,16 @@
 BRAMADDR=0x360
 BRAMDATA=0x361
 
-outb "$BRAMADDR" 0x29 # Select byte at offset 29h
 if [ "$1" = "y" ]; then
-	outb "$BRAMDATA" 0x00 # 0 -> automatic power-on
+	BRAM_VALUE="0x00" # 0 -> automatic power-on
 else
-	outb "$BRAMDATA" 0x01 # 1 -> stay off
+	BRAM_VALUE="0x01" # 1 -> stay off
 fi
 
+outb "$BRAMADDR" 0x29 # Select byte at offset 29h
+outb "$BRAMDATA" "$BRAM_VALUE"
+# There's also a 16-bit checksum at offset 3eh in bank 1.  The only byte
+# included in the checksum is the automatic power-on setting, so the value is
+# the same, and the upper 8 bits remain 0.
+outb "$BRAMADDR" 0x3e
+outb "$BRAMDATA" "$BRAM_VALUE"
