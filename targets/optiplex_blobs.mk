55d7a93e7ea7e54f053e92f021cd79b398bd5fd7
Thierry Laurion <insurgo@riseup.net>
2024-10-07 20:37:06 -0400

    Optiplex 7010/9010 SFF: add ifd.bin from libreboot's t16650 in tree, change optiplex_blobs.mk target and point coreboot config to this IFD
    Signed-off-by: Thierry Laurion <insurgo@riseup.net>
    Signed-off-by: Thierry Laurion <insurgo@riseup.net>

--- targets/optiplex_blobs.mk
+++ targets/optiplex_blobs.mk
@@ -1,9 +1,8 @@
-# Targets for downloading optiplex 7010/9010 ACM blobs
+# Targets for downloading optiplex 7010/9010 blobs: including ACM, SINIT and EC blobs
 
 # Make the Coreboot build depend on the following 3rd party blobs:
 $(build)/coreboot-$(CONFIG_COREBOOT_VERSION)/$(BOARD)/.build: \
-    $(pwd)/blobs/xx30/IVB_BIOSAC_PRODUCTION.bin
-
+    $(pwd)/blobs/xx30/IVB_BIOSAC_PRODUCTION.bin \
 
 $(pwd)/blobs/xx30/IVB_BIOSAC_PRODUCTION.bin:
 	$(pwd)/blobs/xx30/optiplex_7010_9010.sh $(pwd)/blobs/xx30
