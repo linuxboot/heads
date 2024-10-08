8707f1bb189e1777934bb4964555abee8c094510
Thierry Laurion <insurgo@riseup.net>
2024-10-08 14:21:55 -0400

    targets/optiplex_blobs.mk: run extraction script if any of the 3 blobs are not found under blobs/optiplex_9010/
    Signed-off-by: Thierry Laurion <insurgo@riseup.net>
    Signed-off-by: Thierry Laurion <insurgo@riseup.net>

--- targets/optiplex_blobs.mk
+++ targets/optiplex_blobs.mk
@@ -1,8 +1,13 @@
 # Targets for downloading optiplex 7010/9010 blobs: including ACM, SINIT and EC blobs
 
-# Make the Coreboot build depend on the following 3rd party blobs:
-$(build)/coreboot-$(CONFIG_COREBOOT_VERSION)/$(BOARD)/.build: \
+REQUIRED_BLOBS := \
     $(pwd)/blobs/xx30/IVB_BIOSAC_PRODUCTION.bin \
+    $(pwd)/blobs/xx30/SNB_IVB_SINIT_20190708_PW.bin \
+    $(pwd)/blobs/xx30/sch5545_ecfw.bin
 
-$(pwd)/blobs/xx30/IVB_BIOSAC_PRODUCTION.bin:
+# Make the Coreboot build depend on the required blobs
+$(build)/coreboot-$(CONFIG_COREBOOT_VERSION)/$(BOARD)/.build: $(REQUIRED_BLOBS)
+
+# Rule to generate all required blobs
+$(REQUIRED_BLOBS):
 	$(pwd)/blobs/xx30/optiplex_7010_9010.sh $(pwd)/blobs/xx30
