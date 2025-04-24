0dadb67fd199d3eb0e0655965341d25bee41155a
Thomas Clarke <tonux@riseup.net>
2025-04-24 14:06:51 +0100

    Be clear that msi_z790p blobs are for ddr5 only
    Signed-off-by: Thomas Clarke <tonux@riseup.net>

--- targets/msi_z790p_blobs.mk
+++
@@ -1,7 +0,0 @@
-# Make the Coreboot build depend on the following 3rd party blobs:
-$(build)/coreboot-$(CONFIG_COREBOOT_VERSION)/$(BOARD)/.build: \
-    $(pwd)/blobs/msi_z790p/ifd.bin $(pwd)/blobs/msi_z790p/me.bin
-
-$(pwd)/blobs/msi_z790p/ifd.bin $(pwd)/blobs/msi_z790p/me.bin:
-	COREBOOT_DIR="$(build)/$(coreboot_base_dir)" \
-		$(pwd)/blobs/msi_z790p/download_extract.sh
