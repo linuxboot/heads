a0bde5fb08b10fa189360fe586d512d797d829bf
Thomas Clarke <tonux@riseup.net>
2025-04-24 13:54:42 +0100

    Keep MSI layout
    Signed-off-by: Thomas Clarke <tonux@riseup.net>

--- blobs/msi_z790p/download_extract.sh
+++ blobs/msi_z790p/download_extract.sh
@@ -6,7 +6,7 @@
 ami_bios_url="https://download.msi.com/bos_exe/mb/7E06vAG.zip"
 ami_bios_file="E7E06IMS.AG0"
 me_hash="7a33a31cf22ae7e70adcd6f46b848a1e35e030b87fec2e671daf8fb416406396"
-ifd_hash="b6e0010b4bc9797b5e48f62105960267faf0175d246ab9512c1b8eca984744e1"
+ifd_hash="72ddb02b42d2dbb1e3ae745118905941bb4dcd4e68fa082e93598278e7b38259"
 #
 #
 
@@ -38,7 +38,6 @@ rm ./flashregion_1_bios.bin ./flashregion_9_device_exp.bin
 mv ./flashregion_0_flashdescriptor.bin ./ifd.bin
 mv ./flashregion_2_intel_me.bin ./me.bin
 
-$IFDTOOL -p adl ./ifd.bin -n ./layout.txt --output ./ifd.bin
 $IFDTOOL -p adl ./ifd.bin --unlock --output ./ifd.bin
 $IFDTOOL -p adl ./ifd.bin --altmedisable 1 --output ./ifd.bin
 
